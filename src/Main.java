83b8f182ef04c2a61d24a6d80383c4f8f1332d4b
Boris Iurciuc <boris.iurciuc@gmail.com>
2024-02-09 18:09:12 +0100

    edit Main

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
+public class gitMain {
 
   // Class ExchangeList to manage List of objects of transactions
   static ExchangeList exchangeList = new ExchangeList();
